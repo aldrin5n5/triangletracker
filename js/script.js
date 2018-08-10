69c5647da7d18d50b16da9291fc1e2523e6037c2
Aldrin Muhoro Njuguna <aldrin5n5@gmail.com>
2018-08-10 10:31:11 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function check_triangletracker() {;
+var a = parseFloat(prompt("Enter the Length"));
+var b = parseFloat(prompt("Enter the height"));
+var c = parseFloat(prompt("Enter the height"));
+ if (a===b && b===c){
+    alert("This is an equilateral triangle");
+ }
+ else if (a===b||b===c||a===c) {
+   alert("this is an isosceles triangle");
+ }
+ else if ((a+b)<=c ||(b+c)<=a || (a+c)<=b) {
+   alert("this is not a triangle");
+ }
+ else {
+   alert("scalene");
+ }
+ }
